00e87d8645a06d1b06db74478ec9ece5e55dcb18
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-05-27 15:23:48 -0600

    Fix failing tests

--- cqf-ruler/src/main/java/org/opencds/cqf/ruler/Application.java
+++ cqf-ruler/src/main/java/org/opencds/cqf/ruler/Application.java
@@ -2,16 +2,15 @@ package org.opencds.cqf.ruler;
 
 import org.opencds.cqf.ruler.config.CqfRulerConfigR4;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Import;
 
+
+@Import({ca.uhn.fhir.jpa.starter.Application.class, CqfRulerConfigR4.class })
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(
-      ca.uhn.fhir.jpa.starter.Application.class,
-      CqfRulerConfigR4.class);
-
-    app.run(args);
-
+    SpringApplication.run(Application.class, args);
+    
     //Server is now accessible at eg. http://localhost:8080/fhir/metadata
     //UI is now accessible at http://localhost:8080/
   }
